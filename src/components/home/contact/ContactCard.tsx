187060564b206f30a4876815a8a1172e3528f47c
Krumbit <12988040+Krumbit@users.noreply.github.com>
2024-04-11 22:17:22 -0700

    feat: add contact handles

--- src/components/home/contact/ContactCard.tsx
+++ src/components/home/contact/ContactCard.tsx
@@ -10,17 +10,22 @@ interface ContactCardProps {
 export default function ContactCard({ contact, className }: ContactCardProps) {
   return (
     <Link
-      className={`px-auto hoverbg flex h-32 items-center justify-center gap-2 border-2 border-invertbg ${className}`}
+      className={`px-aut o hoverbg group flex h-32 flex-col items-center justify-center border-2 border-invertbg ${className}`}
       href={contact.link}
     >
-      <Image
-        className="aspect-square w-10 invert dark:invert-0"
-        src={contact.image}
-        alt={contact.name}
-        width={100}
-        height={100}
-      />
-      <span className="text-3xl max-md:text-2xl">{contact.name}</span>
+      <div className="flex items-center gap-x-2">
+        <Image
+          className="aspect-square w-10 invert dark:invert-0"
+          src={contact.image}
+          alt={contact.name}
+          width={100}
+          height={100}
+        />
+        <span className="text-3xl max-md:text-2xl">{contact.name}</span>
+      </div>
+      <div className="grid grid-rows-[0fr] transition-[grid-template-rows] duration-250 ease-in-out group-hover:grid-rows-[1fr]">
+        <span className="overflow-hidden text-gray-600 dark:text-gray-400">{contact.handle}</span>
+      </div>
     </Link>
   );
 }
--- src/types.ts
+++ src/types.ts
@@ -23,6 +23,7 @@ export interface Skill {
 export interface Contact {
   name: string;
   link: string;
+  handle: string;
   image: ImageProps["src"];
 }
 
