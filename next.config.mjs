40133ac7b3113cc42a1ad6817ddccbd585e25fb0
Krumbit <12988040+Krumbit@users.noreply.github.com>
2024-09-27 14:22:21 -0700

    revert: next config to mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
-import { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   async rewrites() {
     return [
       {
