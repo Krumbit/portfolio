9c1a571a6f9ad5b3cf7df5750faf8615d38c17cc
Krumbit <12988040+Krumbit@users.noreply.github.com>
2025-02-04 20:46:24 -0800

    chore: remove unused imports

--- src/components/home/skills/SkillsCard.tsx
+++ src/components/home/skills/SkillsCard.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useTheme } from "next-themes";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import { Skill } from "types";
 
